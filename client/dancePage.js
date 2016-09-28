fb1ee6c1b3eee5a8ad746cfd15f745b28527f035
trinaSarkar <tsarkar@stanford.edu>
2016-09-28 11:44:47 -0700

    Dance page

--- client/dancePage.js
+++ client/dancePage.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
      carousel(); 
 });
 var carousel = function() {
+    console.log("hi")
     var i;
     var x = document.getElementsByClassName("danceSlides");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none"; 
     }
-    slideIndex++;
+    slideIndegx++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
     setTimeout(carousel, 2000); // Change image every 2 seconds
